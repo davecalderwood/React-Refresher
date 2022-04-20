88aeee0288e321a8e2a5bd0aec14d23b97291c3f
Dave Calderwood <davidhcalderwood@gmail.com>
2022-04-20 17:12:06 -0400

    Create User Project

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,8 @@
-import './Card.css';
+import React from 'react';
+import styles from './Card.module.css';
 
-function Card(props) {
-    const classes = "card " + props.className;
-
-    return <div className={classes}>{props.children}</div>;
+const Card = props => {
+    return <div className={`${styles.card} ${props.className}`}>{props.children}</div>
 }
 
 export default Card;
