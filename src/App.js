0f2930a40e2bb7da73f5901bc574574b29111976
Dave Calderwood <davidhcalderwood@gmail.com>
2022-04-20 08:59:29 -0400

    Warning fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
