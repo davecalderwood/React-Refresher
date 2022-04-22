bde4d770524727f6576b8c45ca87c69ae3f84664
Dave Calderwood <davidhcalderwood@gmail.com>
2022-04-22 10:56:58 -0400

    Meals App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
-
-function App() {
-  const ctx = useContext(AuthContext);
-
+const App = () => {
   return (
     <React.Fragment>
-        <MainHeader />
-        <main>
-          {!ctx.isLoggedIn && <Login />}
-          {ctx.isLoggedIn && <Home />}
-        </main>
-      </React.Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
   );
 }
 
