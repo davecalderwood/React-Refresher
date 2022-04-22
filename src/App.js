019d3005ae2419d05f1a13a334ee9d4362c61d43
Dave Calderwood <davidhcalderwood@gmail.com>
2022-04-22 13:44:37 -0400

    Adding to cart context via useContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState();
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
